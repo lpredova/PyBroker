bd4237f761bd6779ce71a0f41f44d95d742ae921
Lovro Predovan <lovro.predovan@gmail.com>
2017-06-20 20:32:39 +0200

    Code cleanup and basic comunication infrastructure

--- broker_agent.py
+++ broker_agent.py
@@ -2,144 +2,60 @@
 # !/usr/bin/env python
 import json
 import sys
+import threading
+import time
 
 import spade
 from spade.ACLMessage import ACLMessage
-from spade.Agent import Agent, random, os
+from spade.Agent import Agent, random
 from spade.Behaviour import ACLTemplate, MessageTemplate, Behaviour
 
 
-class TravelerAgent(Agent):
-    class Travel(Behaviour):
-
-        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-        config_file = BASE_DIR + 'agency_config.json'
+class BrokerAgent(Agent):
+    class Speculate(Behaviour):
 
         msg = None
-        #agencies_counter = ConfigurationReader.number_of_agencies()
-        offer_responses = 0
-        offers = []
-
-        ask_discount = random.choice([True, False])
-
-        # Best results in Euro destinations
-        destination = random.choice(['Europe', 'Australia', 'Asia', 'America'])
-
-        budget = random.randint(1000, 50000)
-        persons = random.randint(1, 5)
-
-        discounts_asked = 0
-        discounts_responded = 0
-        discounts_responses = []
+        budget = random.randint(10000, 50000)
 
         def _process(self):
+            print "BROKER"
             self.msg = self._receive(True)
             if self.msg:
                 request = json.loads(self.msg.content)
                 if request['request_type'] == 'offer_response':
-
-                    found_offer = False
-                    self.offer_responses += 1
-                    self.offers.append(request)
-
-                    if self.offer_responses >= self.agencies_counter:
-                        for offer in self.offers:
-                            for o in offer["data"]:
-                                if o["persons"] >= self.persons and o["price"] <= self.budget:
-                                    found_offer = True
-                                    if self.ask_discount:
-                                        print "\nI asking for discount offer from agency %s" % offer["origin"]
-                                        travel = {'request_type': 'discount_request', 'travel_id': o["name"]}
-
-                                        # offer is OK so I'm asking for discount if agent wants discount
-                                        self.send_message(json.dumps(travel), offer["origin"])
-                                        self.discounts_asked += 1
-                                        found_offer = True
-                                    else:
-                                        # agent doesnt want discount, so he books right away
-                                        print "\nI accept offer from agency %s" % offer["origin"]
-                                        book = {'request_type': 'make_booking', 'travel_id': o["name"]}
-                                        self.send_message(json.dumps(book), offer["origin"])
-
-                        if not found_offer:
-                            print "Sorry.There is no offer I'd like to take.\n"
-                            travel = {'request_type': 'not_found'}
-                            self.send_message_all(json.dumps(travel))
-                            sys.exit()
+                    print "msg"
 
                 if request['request_type'] == 'discount_response':
-
-                    self.discounts_responded += 1
-                    self.discounts_responses.append(request)
-                    got_discount = False
-
-                    discounts_got = []
-
-                    if self.discounts_responded == self.discounts_asked:
-                        for discount in self.discounts_responses:
-
-                            if discount["discount_accepted"]:
-                                got_discount = True
-                                print "YAAY! I got discount %i for %s" % (
-                                    discount["discount_amount"], discount["location"])
-                                discounts_got.append(discount)
-
-                        if not got_discount:
-                            print "Sorry.There is no offer I'd like to take.\n"
-                            travel = {'request_type': 'not_found'}
-                            self.send_message_all(json.dumps(travel))
-                            sys.exit()
-
-                    # if there are multiple discounts got, then choose cheapest one
-                    if len(discounts_got) > 0:
-                        cheapest = None
-                        lowest_price = 0
-
-                        for dis in discounts_got:
-                            if dis["discount_amount"] > lowest_price:
-                                cheapest = dis
-
-                        book = {'request_type': 'make_booking', 'travel_id': cheapest["location"]}
-                        self.send_message(json.dumps(book), cheapest["origin"])
+                    print "msg"
 
                 if request['request_type'] == 'booking_confirmed':
-                    print "Great!, I'm traveling to %s" % request['destination']
+                    print "msg"
 
-        def set_preferences(self):
-            print "I'd like to go to %s" % self.destination
-            print "My budget is  %i $ for %i persons" % (self.budget, self.persons)
+        def stop_agent(self):
+            self.kill()
+            sys.exit()
 
-            if self.ask_discount:
-                print "I like discounts\n\n"
-            else:
-                print "I don't want discount\n\n"
+        def broadcast_message(self, content):
 
-            travel = {'request_type': 'travel_request', 'destination': self.destination}
-            self.send_message_all(json.dumps(travel))
+            # for agency_id in agencies_ids:
+            address = "broker%i@127.0.0.1" % 1
+            agent = spade.AID.aid(name=address, addresses=["xmpp://%s" % address])
 
-        def send_message_all(self, content):
+            self.msg = ACLMessage()
+            self.msg.setPerformative("inform")
+            self.msg.setOntology("stock")
+            self.msg.setLanguage("eng")
+            self.msg.addReceiver(agent)
+            self.msg.setContent(content)
+            self.myAgent.send(self.msg)
+            # print '\nMessage %s sent to %s' % (content, address)
 
-            '''agencies_ids = ConfigurationReader.read_agency_id()
-            for agency_id in agencies_ids:
-                address = "agency%i@127.0.0.1" % agency_id
-                agent = spade.AID.aid(name=address, addresses=["xmpp://%s" % address])
-
-                self.msg = ACLMessage()
-                self.msg.setPerformative("inform")
-                self.msg.setOntology("travel")
-                self.msg.setLanguage("eng")
-                self.msg.addReceiver(agent)
-                self.msg.setContent(content)
-                self.myAgent.send(self.msg)
-                # print '\nMessage %s sent to %s' % (content, address)
-            '''
-
-        def send_message(self, content, address):
+        def send_message_to_broker(self, content, address):
 
             agent = spade.AID.aid(name=address, addresses=["xmpp://%s" % address])
             self.msg = ACLMessage()
             self.msg.setPerformative("inform")
-            self.msg.setOntology("travel")
+            self.msg.setOntology("stock")
             self.msg.setLanguage("eng")
             self.msg.addReceiver(agent)
             self.msg.setContent(content)
@@ -149,18 +65,35 @@ class TravelerAgent(Agent):
     def _setup(self):
         print "\n Agent\t" + self.getAID().getName() + " is up"
 
-        feedback_template = ACLTemplate()
-        feedback_template.setOntology('travel')
+        stock_template = ACLTemplate()
+        stock_template.setOntology('stock')
 
-        mt = MessageTemplate(feedback_template)
-        settings = self.Travel()
+        mt = MessageTemplate(stock_template)
+        settings = self.Speculate()
         self.addBehaviour(settings, mt)
-        settings.set_preferences()
+
+
+def start_broker(broker_id):
+    try:
+        ip = "broker%i@127.0.0.1" % broker_id
+        agency_name = "broker%i" % broker_id
+
+        agent = BrokerAgent(ip, agency_name)
+        agent.start()
+
+    except Exception, e:
+        print e
 
 
 if __name__ == '__main__':
-    '''
-        Agent feels like going somewhere (i.e Asia,Russia) and asks agencies to make an offer
-        Ask for initial offer, then select based on current MOOD(price,distance)... and ask for discount, if right then accept offer and say tnx to other agencies
-        '''
-    TravelerAgent('traveler@127.0.0.1', 'traveler').start()
+
+    brokers = [1, 2, 3, 4, 5, 6]
+    for broker in brokers:
+        try:
+            threading.Thread(target=start_broker(broker), args=None).start()
+            time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit()
+        except Exception, e:
+            print "\nError while starting broker!"
+            print e.message
--- stock_exchange_agent.py
+++ stock_exchange_agent.py
@@ -2,8 +2,6 @@
 # !/usr/bin/env python
 import json
 import sys
-import threading
-import time
 
 import spade
 from spade.ACLMessage import ACLMessage
@@ -11,79 +9,58 @@ from spade.Agent import Agent
 from spade.Behaviour import Behaviour, ACLTemplate
 
 
-class AgencyAgent(Agent):
-    class MakingOffer(Behaviour):
+class StockExchange(Agent):
+    class OpenStockExchange(Behaviour):
         msg = None
-        destination = None
 
         def _process(self):
-            self.msg = self._receive(True)
+            try:
+                print "HELLO STOCK"
+                self.msg = self._receive(True)
 
-            '''if self.msg:
-                request = json.loads(self.msg.content)
-                if request['request_type'] == 'travel_request':
-                    #results = ConfigurationReader.destination_finder(request['destination'], self.getName())
-                    print "%s has %i travels to offer" % (self.getName(), len(results))
-                    if len(results) > 0:
-                        for result in results:
-                            print "%s - %i$ : %i persons, %i days" % (
-                                result["name"], result["price"], result["persons"], result["days"])
-                            self.destination = result["name"]
-                    else:
-                        print "There are no destinations to desired location\n"
+                if self.msg:
+                    request = json.loads(self.msg.content)
 
-                    self.send_message(json.dumps(
-                        {'request_type': 'offer_response', 'data': results, 'origin': self.getName().split(" ")[0]}))
+                    if request['request_type'] == 'request_one':
+                        print "msg"
 
-                # calculate if discount is applicable
-                if request['request_type'] == 'discount_request':
-                    #results = ConfigurationReader.discount_finder(request['travel_id'], self.getName())
-                    if len(results) > 0:
-                        for result in results:
-                            if result["name"] == request['travel_id']:
-                                if result["discount"]:
-                                    print "OK, you got yourselves a discount %s for %s" % (
-                                        result["discounted"], result["name"])
+                    if request['request_type'] == 'request_two':
+                        print "msg"
 
-                                    self.send_message(json.dumps(
-                                        {'request_type': 'discount_response', 'discount_accepted': True,
-                                         'discount_amount': result["discounted"],
-                                         'location': result["name"],
-                                         'origin': self.getName().split(" ")[0]}))
+                    if request['request_type'] == 'request_three':
+                        print "msg"
 
-                                else:
-                                    print "Sorry no discount.\n"
-                                    self.send_message(json.dumps(
-                                        {'request_type': 'discount_response', 'discount_accepted': False,
-                                         'discount_amount': None,
-                                         'origin': self.getName().split(" ")[0]}))
+                    if request['request_type'] == 'request_four':
+                        print "msg"
 
                     else:
-                        print "Sorry no discount for %s.\n" % request['travel_id']
-                        self.send_message(json.dumps(
-                            {'request_type': 'discount_response', 'discount_accepted': False, 'discount_ammount': None,
-                             'origin': self.getName().split(" ")[0]}))
+                        print "bump"
+                        pass
 
-                if request['request_type'] == 'make_booking':
-                    print "GREAT! your booking has been made. Have a safe trip\n\n"
-                    self.send_message(
-                        json.dumps({'request_type': 'booking_confirmed', 'destination': self.destination}))
-                    self.kill()
-
-                if request['request_type'] == 'not_found':
-                    print "Sorry! Thats all we can offer\n"
-
-                else:
-                    pass
-                '''
+            except (KeyboardInterrupt, SystemExit):
+                self.stop_agent()
 
         def stop_agent(self):
             self.kill()
             sys.exit()
 
-        def send_message(self, message):
+        def broadcast_message(self, content):
 
-            client = "traveler@127.0.0.1"
+            # for agency_id in agencies_ids:
+            address = "broker%i@127.0.0.1" % 1
+            agent = spade.AID.aid(name=address, addresses=["xmpp://%s" % address])
+
+            self.msg = ACLMessage()
+            self.msg.setPerformative("inform")
+            self.msg.setOntology("stock")
+            self.msg.setLanguage("eng")
+            self.msg.addReceiver(agent)
+            self.msg.setContent(content)
+            self.myAgent.send(self.msg)
+            # print '\nMessage %s sent to %s' % (content, address)
+
+        def send_message(self, message):
+            client = "broker@127.0.0.1"
             address = "xmpp://" + client
             receiver = spade.AID.aid(name=client, addresses=[address])
 
@@ -101,31 +78,14 @@ class AgencyAgent(Agent):
         print "\nTravel agency\t%s\tis up" % self.getAID().getAddresses()
 
         template = ACLTemplate()
-        template.setOntology('travel')
+        template.setOntology('stock')
 
         behaviour = spade.Behaviour.MessageTemplate(template)
-        self.addBehaviour(self.MakingOffer(), behaviour)
-
-
-def start_agency(agency_id):
-    try:
-        ip = "agency%i@127.0.0.1" % agency_id
-        agency_name = "agency0%i" % agency_id
-
-        agent = AgencyAgent(ip, agency_name)
-        agent.start()
-
-    except Exception, e:
-        print e
+        self.addBehaviour(self.OpenStockExchange(), behaviour)
 
 
 if __name__ == "__main__":
-    #agencies_ids = ConfigurationReader.read_agency_id()
-    '''for agency_id in agencies_ids:
-        try:
-            threading.Thread(target=start_agency(agency_id), args=None).start()
-            time.sleep(1)
-        except Exception, e:
-            print "\nError while starting agencies!"
-            print e.message
-    '''
+    try:
+        StockExchange('stock@127.0.0.1', 'stock').start()
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit()
